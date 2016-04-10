24edfbe5cd635838e8f43f4b3359ea43978aa248
relative-rene <rene.arellano415@gmail.com>
2016-04-10 16:35:28 -0700

    app.js attempt #2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,9 +7,8 @@ $('#score-card-form').on('submit', function(event) {
   alert('ScoreCard submit clicked');
   console.log(event);
   event.preventDefault();
-  var formData= $('formData')
+  var formData= $('').serialize();
 });
-
 
 
 
@@ -20,10 +19,12 @@ $('#score-card-form').on('submit', function(event) {
 
 ////event Listener function
 $(".dropdown-menu li a").click(function(event){
-
   $(this).parents(".btn-group").find('.selection').text($(this).text());
   $(this).parents(".btn-group").find('.selection').val($(this).text());
+  console.log(this);
     event.preventDefault();
 });
 
 });
+
+var fighter1, fighter2;
