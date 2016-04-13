08ed5024034ac3cd729d5a6bf7aafd7a24b9924d
relative-rene <rene.arellano415@gmail.com>
2016-04-13 12:45:39 -0700

    fightersController.js linked and organized

--- controllers/fightersController.js
+++ controllers/fightersController.js
@@ -14,8 +14,8 @@ function create(req, res) {
   console.log('body', req.body);
 
   // split at comma and remove and trailing space
-  var genres = req.body.genres.split(',').map(function(item) { return item.trim(); } );
-  req.body.genres = genres;
+  // var genres = req.body.genres.split(',').map(function(item) { return item.trim(); } );
+  // req.body.genres = genres;
 
   db.Fighter.create(req.body, function(err, fighter) {
     if (err) { console.log('error', err); }
@@ -25,17 +25,17 @@ function create(req, res) {
 }
 
 function show(req, res) {
-  db.Fighter.findById(req.params.albumId, function(err, foundFighter) {
+  db.Fighter.findById(req.params.fighterId, function(err, foundFighter) {
     if(err) {
-      console.log('albumsController.show', err);
+      console.log('fightersController.show', err);
     }
-    console.log('albumsController.show responding with', foundFighter);
+    console.log('fightersController.show responding with', foundFighter);
     res.json(foundFighter);
   });
 }
 
 function destroy(req, res) {
-  db.Fighter.findOneAndRemove({ _id: req.params.albumId }, function(err,
+  db.Fighter.findOneAndRemove({ _id: req.params.fighterId }, function(err,
 foundFighter) {
   // note you could send just send 204, but we're sending 200 and the deleted entity
   re.json(foundFighter);
@@ -44,14 +44,14 @@ foundFighter) {
 
 function update(req, res) {
   console.log('updating with data', req.body);
-  db.Fighter.findById(req.params.albumId, function(err, foundFighter) {
-    if(err) { console.log('albumsController.update error', err); }
-    foundFighter.artistName = req.body.artistName;
-    foundFighter.name = req.body.name;
-    foundFighter.releaseDate = req.body.releaseDate;
-    foundFighter.save(function(err, savedAlbum) {
+  db.Fighter.findById(req.params.fighterId, function(err, foundFighter) {
+    if(err) { console.log('fightersController.update error', err); }
+    // foundFighter.artistName = req.body.artistName;
+    // foundFighter.name = req.body.name;
+    foundFighter.proDebut = req.body.proDebut;
+    foundFighter.save(function(err, savedFighter) {
       if(err) { console.log('saving altered fighter failed'); }
-      res.json(savedAlbum);
+      res.json(savedFighter);
     });
   });
 
