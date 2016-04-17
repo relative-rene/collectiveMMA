34aa1cc366e54a9fe2f830098c41628679147f40
relative-rene <rene.arellano415@gmail.com>
2016-04-17 12:32:19 -0700

    cleaned up the html, improving the UI

--- public/scripts/appPieces.js
+++ public/scripts/appPieces.js
@@ -5,63 +5,7 @@ var template;
 $(document).ready(function() {
 
 
-  $.get('/api/fighters').success(function (fighters) {
-    fighters.forEach(function(fighter) {
-      renderFighter(fighter);
-    });
-  });
-createAfighter();
-  });
 
-function createAfighter(){
-    $('#fighterBuilder-form').on('submit','#signAfighter', function(e) {
-        e.preventDefault();
-        console.log(e);
-        var formData = $(this).serialize();
-        console.log('formData', formData);
-        $.post('/api/fighters', formData, function(fighter) {
-          console.log('fighter after POST', fighter);
-          renderFighter(fighter);  //render the server's response
-        });
-        $(this).trigger("reset");
-  });//doc ready ends=-----------
-}
-
-
-function handleNewEventSubmit(e) {
-  e.preventDefault();
-  var $modal = $('#eventModal');
-  var $eventNameField = $modal.find('#eventName');
-  var $trackNumberField = $modal.find('#trackNumber');
-
-  // get data from modal fields
-  // note the server expects the keys to be 'name', 'trackNumber' so we use those.
-  var dataToPost = {
-    name: $eventNameField.val(),
-    trackNumber: $trackNumberField.val()
-  };
-  var fighterId = $modal.data('fighterId');
-  console.log('retrieved eventName:', eventName, ' and trackNumber:', trackNumber, ' for fighter w/ id: ', fighterId);
-  // POST to SERVER
-  var eventPostToServerUrl = '/api/fighters/'+ fighterId + '/events';
-  $.post(eventPostToServerUrl, dataToPost, function(data) {
-    console.log('received data from post to /events:', data);
-    // clear form
-    $eventNameField.val('');
-    $trackNumberField.val('');
-
-    // close modal
-    $modal.modal('hide');
-    // update the correct fighter to show the new event
-    $.get('/api/fighters/' + fighterId, function(data) {
-      // remove the current instance of the fighter from the page
-      $('[data-fighter-id=' + fighterId + ']').remove();
-      // re-render it with the new fighter data (including events)
-      renderFighter(data);
-    });
-  }).error(function(err) {
-    console.log('post to /api/fighters/:fighterId/events resulted in error', err);
-  });
 }
 
 
@@ -101,45 +45,23 @@ function handleNewEventSubmit(e) {
   // });
 
 
-
-event Listener function
-  $(".dropdown-menu li a").click(function(event){
-    var $linkText= $(this).text();
-    $(this).parents(".btn-group").find('.selection').text($linkText);
-    console.log(this);
-    event.preventDefault();
-});
-
-});
-
-$('#total').html("<h3>"+'fighter 2'+ fighter2total+'fighter 1'+"</h3>");
-function fighterDisplay(){
-  $.ajax({
-    url:'http://ufc-data-api.ufc.com/api/v1/us/fighters',
-    method: 'POST',
-    success:
-    error:function(err){
-      console.log(err)
-    }
-  });
-
-
-function fighter1Score(){
-  var fighter1=0;
-$(".dropdown-menu li a").click(function(event){
-            $(this).parents(".btn-group1").find('.selection').val($(this).text());
-event.fighter1 += +$(this).parents(".btn-group1").find('.selection').text($(this).text());
-              console.log(fighter1);
-              event.preventDefault();
-            });
-          }
-
-function fighter2Score(){
-  var fighter2=0;
-  $(".dropdown-menu li a").click(function(event){
-            $(this).parents(".btn-group2").find('.selection').val($(this).text());
-event.fighter2 += +$(this).parents(".btn-group2").find('.selection').text($(this).text());
-              event.preventDefault();
-              console.log(fighter2);
-            });
-          }
+// 
+// function fighter1Score(){
+//   var fighter1=0;
+// $(".dropdown-menu li a").click(function(event){
+//             $(this).parents(".btn-group1").find('.selection').val($(this).text());
+// event.fighter1 += +$(this).parents(".btn-group1").find('.selection').text($(this).text());
+//               console.log(fighter1);
+//               event.preventDefault();
+//             });
+//           });
+//
+// function fighter2Score(){
+//   var fighter2=0;
+//   $(".dropdown-menu li a").click(function(event){
+//             $(this).parents(".btn-group2").find('.selection').val($(this).text());
+// event.fighter2 += +$(this).parents(".btn-group2").find('.selection').text($(this).text());
+//               event.preventDefault();
+//               console.log(fighter2);
+//             });
+//           }
