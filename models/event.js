57912dc0b42ddb1831d8fb0ea8946aa8c6e7cd83
relative-rene <rene.arellano415@gmail.com>
2016-04-20 10:18:05 -0700

    listener issues with save event

--- models/event.js
+++ models/event.js
@@ -7,7 +7,7 @@ var eventSchema = new Schema({
   arena:String,
   date:String,
   city:String,
-  opponent:String
+  oppnent:String
 });
 
 module.exports = mongoose.model('Event', eventSchema);
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,70 +1,3 @@
-
-//The below renders the entire fighter list on index.html
-$(document).ready(function() {
-  console.log('app.js loaded!');
-  //working functions
-
-  $.get('/api/fighters').success(function (fighters) {
-    fighters.forEach(function(fighter) {
-      renderFighter(fighter);
-    });
-  });
-
-//event listener
-  function handleDeleteFighterClick(e) {
-    var fighterId = $(this).parents('.fighter').data('fighter-id');
-    console.log('someone wants to delete fighter id=' + fighterId );
-    $.ajax({
-      url: '/api/fighters/' + fighterId,
-      method: 'DELETE',
-      success: handleDeleteFighterSuccess
-    });
-  }
-
-  // callback after DELETE /api/fighters/:id
-  function handleDeleteFighterSuccess(data) {
-    var deletedFighterId = data._id;
-    console.log('removing the following fighter from the page:', deletedFighterId);
-    $('div[data-fighter-id=' + deletedFighterId + ']').remove();
-  }
-
-
-//working listeners
-  $('#fighters').on('click', '.delete-fighter', handleDeleteFighterClick);
-  $('#fighters').on('click', '.edit-fighter', handleFighterEditClick);
-  $('#fighters').on('click', '.add-event', handleAddEventClick);
-
-
-  $('#fighterBuilder-form form').on('submit', function(e) {
-    e.preventDefault();
-    var formData = $(this).serialize();
-    console.log('formData', formData);
-    $.post('/api/fighters', formData, function(fighter) {
-      console.log('fighter after POST', fighter);
-      renderFighter(fighter);  //render the server's response
-    });
-    $(this).trigger("reset");
-  });
-
-  // catch and handle the click on an add event button
-
-  // save event modal save button
-  $('#saveEvent').on('click', handleNewEventSubmit);
-  $('#fighters').on('click', '.save-fighter', handleSaveChangesClick);
-  $('#fighters').on('click', '.edit-events', handleEditEventsClick);
-});
-
-// when edit events button clicked
-function handleEditEventsClick(e) {
-  var $fighterRow = $(this).closest('.fighter');
-  var fighterId = $fighterRow.data('fighter-id');
-  console.log('edit events clicked for ', fighterId);
-
-  // fire zee modal!
-  $('#editEventsModal').modal();
-
-}
-
 // when the edit button for an fighter is clicked
 function handleFighterEditClick(e) {
   var $fighterRow = $(this).closest('.fighter');
@@ -130,7 +63,6 @@ function handleFighterUpdatedResponse(data) {
 
 
 
-
 // this function takes a single fighter and renders it to the page
 function renderFighter(fighter) {
   console.log('rendering fighter', fighter);
@@ -140,49 +72,116 @@ function renderFighter(fighter) {
   $('#fighters').prepend(html);
 }
 
-// when the add event button is clicked, display the modal
-function handleAddEventClick(e) {
-  console.log('add-event clicked!');
-  var currentFighterId = $(this).closest('.fighter').data('fighter-id'); // "5665ff1678209c64e51b4e7b"
-  console.log('id',currentFighterId);
-  $('#eventModal').data('fighter-id', currentFighterId);
-  $('#eventModal').modal();  // display the modal!
+
+//The below renders the entire fighter list on index.html
+// when edit events button clicked
+function handleEditEventsClick(e) {
+  var $fighterRow = $(this).closest('.fighter');
+  var fighterId = $fighterRow.data('fighter-id');
+  console.log('edit events clicked for ', fighterId);
+
+  // fire zee modal!
+  $('#editEventsModal').modal();
+
 }
 
-// when the event modal submit button is clicked:
-function handleNewEventSubmit(e) {
-  e.preventDefault();
-  var $modal = $('#eventModal');
-  var $date = $modal.find('#date');
-  var $opponent = $modal.find('#opponent');
-
-  // get data from modal fields
-  // note the server expects the keys to be 'name', 'opponent' so we use those.
-  var formPost = {
-    name: $date.val(),
-    opponent: $opponent.val()
-  };
-  var fighterId = $modal.data('fighterId');
-  console.log('retrieved date:', date, ' and opponent:', opponent, ' for fighter w/ id: ', fighterId);
-  // POST to SERVER
-  var eventPostToServerUrl = '/api/fighters/'+ fighterId + '/events';
-
-  $.post(eventPostToServerUrl, formPost, function(data) {
-    console.log('received data from post to /events:', data);
-    // clear form
-    $date.val('');
-    $opponent.val('');
-
-    // close modal
-    $modal.modal('hide');
-    // update the correct fighter to show the new event
-    $.get('/api/fighters/' + fighterId, function(data) {
-      // remove the current instance of the fighter from the page
-      $('[data-fighter-id=' + fighterId + ']').remove();
-      // re-render it with the new fighter data (including events)
-      renderFighter(data);
+//event listener
+  function handleDeleteFighterClick(e) {
+    var fighterId = $(this).parents('.fighter').data('fighter-id');
+    console.log('someone wants to delete fighter id=' + fighterId );
+    $.ajax({
+      url: '/api/fighters/' + fighterId,
+      method: 'DELETE',
+      success: handleDeleteFighterSuccess
     });
-  }).error(function(err) {
-    console.log('post to /api/fighters/:fighterId/events resulted in error', err);
+  }
+  // callback after DELETE /api/fighters/:id
+  function handleDeleteFighterSuccess(data) {
+    var deletedFighterId = data._id;
+    console.log('removing the following fighter from the page:', deletedFighterId);
+    $('div[data-fighter-id=' + deletedFighterId + ']').remove();
+  }
+
+  // when the add event button is clicked, display the modal
+  function handleAddEventClick(e) {
+    console.log('add-event clicked!');
+    var currentFighterId = $(this).closest('.fighter').data('fighter-id'); // "5665ff1678209c64e51b4e7b"
+    console.log('id',currentFighterId);
+    $('#eventModal').data('fighter-id', currentFighterId);
+    $('#eventModal').modal();  // display the modal!
+  }
+
+  // when the event modal submit button is clicked:
+  function handleNewEventSubmit(e) {
+    e.preventDefault();
+    var $modal = $('#eventModal');
+    var $date = $modal.find('#date');
+    var $opponent = $modal.find('#opponent');
+
+    // get data from modal fields
+    // note the server expects the keys to be 'name', 'opponent' so we use those.
+    var formPost = {
+      name: $date.val(),
+      opponent: $opponent.val()
+    };
+    var fighterId = $modal.data('fighterId');
+    console.log('retrieved date:', date, ' and opponent:', opponent, ' for fighter w/ id: ', fighterId);
+    // POST to SERVER
+    var eventPostToServerUrl = '/api/fighters/'+ fighterId + '/events';
+
+    $.post(eventPostToServerUrl, formPost, function(data) {
+      console.log('received data from post to /events:', data);
+      // clear form
+      $date.val('');
+      $opponent.val('');
+
+      // close modal
+      $modal.modal('hide');
+      // update the correct fighter to show the new event
+      $.get('/api/fighters/' + fighterId, function(data) {
+        // remove the current instance of the fighter from the page
+        $('[data-fighter-id=' + fighterId + ']').remove();
+        // re-render it with the new fighter data (including events)
+        renderFighter(data);
+      });
+    }).error(function(err) {
+      console.log('post to /api/fighters/:fighterId/events resulted in error', err);
+    });
+  }
+
+$(document).ready(function() {
+  console.log('app.js loaded!');
+
+  $('#fighterBuilder-form form').on('submit', function(e) {
+    e.preventDefault();
+    var formData = $(this).serialize();
+    console.log('formData', formData);
+    $.post('/api/fighters', formData, function(fighter) {
+      console.log('fighter after POST', fighter);
+      renderFighter(fighter);  //render the server's response
+    });
+    $(this).trigger("reset");
   });
-}
+
+  //working functions
+
+  $.get('/api/fighters').success(function (fighters) {
+    fighters.forEach(function(fighter) {
+      renderFighter(fighter);
+    });
+  });
+
+//working listeners
+  $('#fighters').on('click', '.delete-fighter', handleDeleteFighterClick);
+  $('#fighters').on('click', '.edit-fighter', handleFighterEditClick);
+  $('#fighters').on('click', '.add-event', handleAddEventClick);
+  $('#fighters').on('click', '.save-fighter', handleSaveChangesClick);
+  $('#saveEvent').on('click',handleNewEventSubmit);
+  $('#fighters').on('click', '.edit-event', handleEditEventsClick);
+
+
+
+});
+  // catch and handle the click on an add event button
+
+  // save event modal save button
