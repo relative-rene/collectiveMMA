16a18fed61d3910e61777f66e926a02e75dad45f
relative-rene <rene.arellano415@gmail.com>
2016-08-30 16:57:58 -0700

    bash checkin

--- models/index.js
+++ models/index.js
@@ -1,9 +1,6 @@
 var mongoose = require("mongoose");
-var MONGOLAB_CYAN_URI = "mongodb://heroku_726br2gj:dtqogtbbgfk4vuk1shrkahj5al@ds019946.mlab.com:19946/heroku_726br2gj";
-mongoose.connect(MONGOLAB_CYAN_URI || 'mongodb://localhost:3000/thescoreapp');
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
+var db = require('../models');
+mongoose.connect(process.env.MONGOLAB_CYAN_URI || 'mongodb://localhost/thescoreapp');
 
 module.exports.Fighter = require('./fighter.js');
 module.exports.Judge = require('./judge.js');
@@ -11,5 +8,3 @@ module.exports.Referee = require('./referee.js');
 module.exports.User = require('./user.js');
 module.exports.ScoreCard = require('./scoreCard.js');
 module.exports.Event = require('./event.js');
-
-});
