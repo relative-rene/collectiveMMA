1dab20be64f32593aa966c4507a423b51d8435d8
relative-rene <rene.arellano415@gmail.com>
2016-08-30 15:03:55 -0700

    saved

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI ||
+mongoose.connect(MONGODB_URI ||
                   "mongodb://localhost/thescoreapp");
 
 module.exports.Fighter = require('./fighter.js');
