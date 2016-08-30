5b614ccb8e56307360f92beca127047b9e05574f
relative-rene <rene.arellano415@gmail.com>
2016-08-29 22:47:19 -0700

    adds mongolab

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/thescoreapp");
+
+var mongodbUri = 'mongodb://user:pass@host:port/db';
+
+mongoose.connect(mongodbUri);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback () {
 
 module.exports.Fighter = require('./fighter.js');
 module.exports.Judge = require('./judge.js');
@@ -7,3 +16,4 @@ module.exports.Referee = require('./referee.js');
 module.exports.User = require('./user.js');
 module.exports.ScoreCard = require('./scoreCard.js');
 module.exports.Event = require('./event.js');
+});
