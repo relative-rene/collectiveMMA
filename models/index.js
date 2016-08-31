afb96a13dec58683a96e6efb21b2e26c163c827c
relative-rene <rene.arellano415@gmail.com>
2016-08-30 17:21:23 -0700

    saves

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
-var mongodbUri = "mongodb://heroku_726br2gj:dtqogtbbgfk4vuk1shrkahj5al@ds019946.mlab.com:19946/heroku_726br2gj";
-mongoose.connect(mongodbUri);
+var MONGOLAB_CYAN_URI = "mongodb://heroku_726br2gj:dtqogtbbgfk4vuk1shrkahj5al@ds019946.mlab.com:19946/heroku_726br2gj";
+mongoose.connect( MONGOLAB_CYAN_URI ||
+                  process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  "mongodb://localhost/thescoreapp");
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
