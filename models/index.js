fa489bf4a7c1a7bbf8d85a828cd101c5169f7721
relative-rene <rene.arellano415@gmail.com>
2016-08-30 17:03:46 -0700

    chore

--- models/index.js
+++ models/index.js
@@ -1,6 +1,11 @@
-var mongoose = require("mongoose");
-var db = require('../models');
-mongoose.connect(process.env.MONGOLAB_CYAN_URI || 'mongodb://localhost/thescoreapp');
+var mongoose = require('mongoose');
+var mongodbUri = 'mongodb://heroku_726br2gj:dtqogtbbgfk4vuk1shrkahj5al@ds019946.mlab.com:19946/heroku_726br2gj';
+mongoose.connect(mongodbUri);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback () {
 
 module.exports.Fighter = require('./fighter.js');
 module.exports.Judge = require('./judge.js');
@@ -8,3 +13,4 @@ module.exports.Referee = require('./referee.js');
 module.exports.User = require('./user.js');
 module.exports.ScoreCard = require('./scoreCard.js');
 module.exports.Event = require('./event.js');
+});
